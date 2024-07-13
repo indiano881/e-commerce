3b9a9c4419231c41ca574109d40482c312f690e3
Davide Baldi <d.baldi88@gmail.com>
2024-07-13 19:30:00 +0200

    save with link outside

--- server/db.js
+++ server/db.js
@@ -1,12 +1,10 @@
 import mongoose from "mongoose";
-
+import dotenv from 'dotenv';
+dotenv.config();
 export const connectToDatabase = async () => {
     try {
         mongoose.set("strictQuery", false)
-        const connect= await mongoose.connect(process.env.MONGO_URI,{
-            useUnifiedTopology: true,
-            useNewUrlParser: true
-        })
+        const connect= await mongoose.connect("mongodb+srv://d.baldi88:Changemaker.01@cluster0.gjnwqd3.mongodb.net/")
         console.log(`MongoDb Connected: ${connect.connection.host}`)
     } catch (error) {
         console.log(`Error: ${error.message}`)
