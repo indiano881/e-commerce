5a6d3b0afec2e3cc5c89c8d143a85c9162a7d7a7
Davide Baldi <d.baldi88@gmail.com>
2024-07-14 17:06:09 +0200

    save

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,6 +1,8 @@
 import {combineReducers, configureStore} from "@reduxjs/toolkit";
+import product from "./slices/product";
 
-
-const reducer= combineReducers({})
+const reducer= combineReducers({
+    product
+})
 
 export default configureStore({reducer})
