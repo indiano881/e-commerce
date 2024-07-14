29555f7123706b5eabd7776b0095099ad2a6b725
Davide Baldi <d.baldi88@gmail.com>
2024-07-14 11:29:14 +0200

    basic chakra UI card componjent created

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import ProductsScreen from './screens/ProductsScreen';
 
 function App() {
   return (
     <ChakraProvider>
       <h1>frontend</h1>
+
+      <ProductsScreen />
     </ChakraProvider>
   );
 }
