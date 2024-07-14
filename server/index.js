fa6395f32e92fe191af18083171708b060fe25de
Davide Baldi <d.baldi88@gmail.com>
2024-07-14 09:57:30 +0200

    basic routing config

--- server/index.js
+++ server/index.js
@@ -2,6 +2,11 @@
 import {connectToDatabase} from "./db.js"
 import express from "express"
 import cors from "cors"
+//Routes
+import productRoutes from "./routes/productroutes.js"
+
+
+
 
 connectToDatabase();
 
@@ -9,6 +14,8 @@ const app =express();
 
 app.use(express.json())
 app.use(cors())
+
+app.use("/api/products", productRoutes)
 
 const port = 5000;
 app.get('/',  (req, res)=> {
