f94efd3a3b14aa7c807224cdd285f1deacac0c6d
Davide Baldi <d.baldi88@gmail.com>
2024-07-14 09:40:57 +0200

    schema created

---
+++ server/models/Product.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+
+const productSchema= new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    images: {
+        type: Array,
+        required: true,
+        default: []
+    },
+    brand: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    reviews: {
+        type: Array,
+        required: true,
+        default: []
+    },
+    rating: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    numberOfReview: {
+        type: Number,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    stock: {
+        type: Number,
+        required: true
+    },
+    productIsNew: {
+        type: Boolean,
+        required: true
+    },
+    stripeID: {
+        type: String
+    },
+    
+})
