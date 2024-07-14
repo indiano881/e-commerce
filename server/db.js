4c7a640b8aac55dcc7471d7fc682fe89753ff6eb
Davide Baldi <d.baldi88@gmail.com>
2024-07-14 09:27:29 +0200

    FUCKING DATABSE IS WORKING NOW

--- server/db.js
+++ server/db.js
@@ -4,7 +4,7 @@ dotenv.config();
 export const connectToDatabase = async () => {
     try {
         mongoose.set("strictQuery", false)
-        const connect= await mongoose.connect("mongodb+srv://d.baldi88:Changemaker.01@cluster0.gjnwqd3.mongodb.net/")
+        const connect= await mongoose.connect(process.env.MONGO_URI)
         console.log(`MongoDb Connected: ${connect.connection.host}`)
     } catch (error) {
         console.log(`Error: ${error.message}`)
