8b72144c5f57ca99da7962179cbea8673deea935
Davide Baldi <d.baldi88@gmail.com>
2024-07-14 16:45:31 +0200

    redux slice completed

---
+++ client/src/redux/slices/product.js
@@ -0,0 +1,39 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const initialState = {
+    loading: false,
+    error: null,
+    products: [],
+    pagination: {},
+    favouritesToogle: true,
+}
+
+export const productSlice= createSlice({
+    name: "products",
+    initialState,
+    reducers: {
+        setLoading: (state) => {
+            state.loading = true;
+        },
+        setProducts: (state, {payload}) => {
+            state.loading = false;
+            state.error = null;
+            state.products = payload;
+        },
+        setError: (state, {payload})=> {
+            state.loading= false;
+            state.error = payload;
+        },
+        setPagination: (state, {payload})=> {
+            state.loading = false;
+            state.error = null;
+            state.pagination = payload;
+        }
+    }
+})
+
+export const {setLoading,setError,setPagination,setProducts} =productSlice.actions;
+
+export default productSlice.reducer;
+
+export const productSelector= (state) => state.products;
