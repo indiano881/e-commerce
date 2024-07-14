270c60b92b4af2d48dadb881f521ffbe730f5a95
Davide Baldi <d.baldi88@gmail.com>
2024-07-14 11:57:30 +0200

    save cards

--- client/src/screens/ProductsScreen.jsx
+++ client/src/screens/ProductsScreen.jsx
@@ -1,4 +1,4 @@
-import { Box, Wrap } from "@chakra-ui/react"
+import { Box, Center, Wrap, WrapItem } from "@chakra-ui/react"
 import ProductCard from "../components/ProductCard";
 import axios from "axios"
 import { useEffect, useState } from "react";
@@ -23,7 +23,16 @@ const ProductsScreen = () => {
         <> {data.length > 1 && (
             <Box>
                 <Wrap spacing="30px" justify="center" minHeight="80vh" mx={{base: "12", md: "20", lg: "32"}} >
-                <ProductCard product={data[0]} loading={false} />
+                
+                {data.map((item, index) =>(
+                    <WrapItem key={item.id}>
+                        <Center w={"250px"} h={"450px"}>
+                            <ProductCard product={item} loading={false} />
+                        </Center>
+                    </WrapItem>
+
+
+                ))}
                 </Wrap>
             </Box>
 
